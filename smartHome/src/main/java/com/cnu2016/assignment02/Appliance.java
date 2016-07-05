acafcd37e3b40a1caf149af63567d6b43b907a3a
pranet <pranetverma@gmail.com>
2016-07-05 11:08:45 +0000

    first commit

--- smartHome/src/main/java/com/cnu2016/assignment02/Appliance.java
+++
@@ -1,8 +0,0 @@
-package com.cnu2016.assignment02;
-
-abstract public class Appliance {
-    boolean isOn;
-    int maxOnTime;
-    abstract void start();
-    abstract void stop();
-}
--- smartHome/src/main/java/com/cnu2016/assignment02/Main.java
+++
@@ -1,7 +0,0 @@
-package com.cnu2016.assignment02;
-
-public class Main {
-    public static void main(String args[]) {
-        System.out.println("a");
-    }
-}
