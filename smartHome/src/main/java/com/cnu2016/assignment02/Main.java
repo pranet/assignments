a4c6b0ff88b0fc46a49a02569eb8a0e22703b137
pranet <pranetverma@gmail.com>
2016-07-05 12:39:26 +0000

    Adding input reader

--- smartHome/src/main/java/com/cnu2016/assignment02/Main.java
+++ smartHome/src/main/java/com/cnu2016/assignment02/Main.java
@@ -1,7 +1,31 @@
 package com.cnu2016.assignment02;
-
+import java.io.*;
+import java.util.Scanner;
 public class Main {
-    public static void main(String args[]) {
-        System.out.println("a");
+    private static void readData(String filename) throws IOException {
+        Scanner in = new Scanner(new FileReader(filename));
+        while(in.hasNext()) {
+            int ID, startTime, duration;
+            try {
+                ID = in.nextInt();
+                startTime = in.nextInt();
+                duration = in.nextInt();    
+                return ID + startTime + duration;
+            }
+            catch (Exception e) {
+                
+            }
+            finally {
+                in.close();
+            }
+        }
     }
+    public static void main(String args[]) throws IOException {
+        readData("input.txt");
+    }
+
+    
+//
+//writer.println("The second line");
+//writer.close();
 }
