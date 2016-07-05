829feda0447151c44e5676cd15707c8470732d0d
pranet <pranetverma@gmail.com>
2016-07-05 17:36:29 +0000

    Unit tests for Appliance class added

--- smartHome/src/test/java/com/cnu2016/assignment02/ApplicanceTest.java
+++ smartHome/src/test/java/com/cnu2016/assignment02/ApplicanceTest.java
@@ -1,4 +1,18 @@
 package com.cnu2016.assignment02;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class ApplicanceTest {
+    @Test
+    public void testBasics() {
+        Appliance a = new Appliance(Appliance.Type.WATERHEATER);
+        Appliance b = new Appliance(Appliance.Type.COOKINGOVEN);
+        assertEquals(a.getID() + 1,b.getID());
+        assertEquals(a.getType(), Appliance.Type.WATERHEATER);
+        assertEquals(a.getType(), Appliance.Type.COOKINGOVEN);
+        assertEquals(a.getState(), false);
+        a.start();
+        assertEquals(a.getState(), true);
+        a.stop();
+        assertEquals(a.getState(), false);
+    }
 }
