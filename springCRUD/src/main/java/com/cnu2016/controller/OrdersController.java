4aeff8e6f8ea6bd57bbd22387f2e9d74c16550c2
Pranet Verma <pranet@codenation.co.in>
2016-07-13 13:52:10 +0400

    fixes

--- springCRUD/src/main/java/com/cnu2016/controller/OrdersController.java
+++ springCRUD/src/main/java/com/cnu2016/controller/OrdersController.java
@@ -136,7 +136,7 @@ public class OrdersController {
         order.setOrderDate(new Date());
 
         ordersRepository.save(order);
-        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+        return ResponseEntity.status(HttpStatus.OK).body(order);
     }
 
     /**
