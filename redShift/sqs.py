0c5786035b10977b58d75e66fa49ee56e9bc7851
Pranet Verma <pranet@codenation.co.in>
2016-07-18 11:55:25 +0400

    Yet to do logging

--- redShift/sqs.py
+++ redShift/sqs.py
@@ -5,18 +5,11 @@ import time
 import config
 from sqlalchemy import *
 
-
-# Connect to database
-metadata = MetaData()
-db = create_engine("mysql+pymysql://" + config.username + ":" + config.password + "@" + config.host + "/" + config.database);
-connection = db.connect()
-
-def populateAuditLog(row):
+def populate_audit_log(row):
     timestamp = row['timeStamp']
     timestamp = datetime.datetime(timestamp['weekyear'], timestamp['monthOfYear'], timestamp['dayOfMonth'], timestamp['hourOfDay'],
                       timestamp['minuteOfHour'], timestamp['secondOfMinute']).isoformat()
     table = Table('audit_log', metadata, autoload=True, autoload_with=db)
-    assert table != None
     ins = table.insert().values(url=row['url'],
                               parameters=row['parametersString'],
                               responseCode=row['id'],
@@ -24,16 +17,26 @@ def populateAuditLog(row):
                               timestamp=timestamp)
     connection.execute(ins)
 
-# Connect to SQS queue
-conn = boto.sqs.connect_to_region(config.region)
-Q = conn.get_queue(config.queueName)
-while True:
-    m = Q.read()
-    if m == None:
-        print 'No data found. Sleeping for 5s'
-        time.sleep(5)
-    else:
-        print 'Inserting' + m.get_body();
-        dict = json.loads(m.get_body())
-        populateAuditLog(dict)
-        m.delete()
+def run():
+    while True:
+        m = Q.read()
+        if m == None:
+            print 'No data found. Sleeping for 5s'
+            time.sleep(5)
+        else:
+            print 'Inserting' + m.get_body();
+            dict = json.loads(m.get_body())
+            populate_audit_log(dict)
+            m.delete()
+
+if __name__ == '__main__':
+    # Connect to database
+    metadata = MetaData()
+    db = create_engine(
+        "mysql+pymysql://" + config.username + ":" + config.password + "@" + config.host + "/" + config.database);
+    connection = db.connect()
+    # Connect to SQS queue
+    conn = boto.sqs.connect_to_region(config.region)
+    Q = conn.get_queue(config.queueName)
+
+    run()
